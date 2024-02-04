7425791ade46c6700d99e293bfe855754adcbd40
fizkers <158344155+fizkers@users.noreply.github.com>
2024-02-04 23:33:00 +0700

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+document.getElementById("pesanButton").addEventListener("click", function () {
+  var pesan = prompt(
+    "Tanyoe ka rayeuk bek manja le wkwk, dan ikah bek gadoh kamaen slot bahaya enteuk ikah ka pinjol lom kon kop rusak kah hahahaha, jadi ka beri impian kah untuk impian kah di thon nyoe, kapasoe bak kolom dimiyub"
+  );
+  document.getElementById("pesanContainer").innerText = pesan;
+});
